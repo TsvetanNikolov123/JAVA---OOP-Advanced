4322279c63e56fe6fe536659449334881afbcada
Tsvetan <tsdman1985@gmail.com>
2018-07-21 20:58:14 +0300

    add solution to last tasks of exercise

---
+++ 8 EXERCISE ENUMS AND ANNOTATIONS/p11_Inferno_Infinity_Command_Pattern/io/ConsoleOutputWriter.java
@@ -0,0 +1,13 @@
+package p11_Inferno_Infinity_Command_Pattern.io;
+
+public class ConsoleOutputWriter implements OutputWriter {
+    @Override
+    public void writeLine(String output) {
+        System.out.println(output);
+    }
+
+    @Override
+    public void writeLine(String format, Object... params) {
+        System.out.println(String.format(format, params));
+    }
+}
