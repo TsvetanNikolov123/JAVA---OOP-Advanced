b691265f070b7e9abadfa3cc0c8ccc84c314ad9a
Tsvetan <tsdman1985@gmail.com>
2018-07-26 08:18:24 +0300

    add solution to The Commands Strike Back tasks

---
+++ 10 EXERCISE REFLECTION/pr04_BarracksWars_The_Commands_Strike_Back/pr0304Barracks/core/commands/AddCommand.java
@@ -0,0 +1,22 @@
+package pr0304Barracks.core.commands;
+
+import pr0304Barracks.contracts.Repository;
+import pr0304Barracks.contracts.Unit;
+import pr0304Barracks.contracts.UnitFactory;
+
+public class AddCommand extends BaseCommand {
+
+    public AddCommand(String[] data, Repository repository, UnitFactory unitFactory) {
+        super(data, repository, unitFactory);
+    }
+
+    @Override
+    public String execute() {
+        String unitType = super.getData()[1];
+        Unit unitToAdd = super.getUnitFactory().createUnit(unitType);
+
+        super.getRepository().addUnit(unitToAdd);
+
+        return unitType + " added!";
+    }
+}
